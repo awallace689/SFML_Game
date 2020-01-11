b5cac96094664a1c0c3252e201ea29566c5ed097
Adam Wallacessh-keygen -t rsa -b 4096 -C awallace689@gmail.com <awallace689@gmail.com>
2020-01-10 21:18:40 -0600

    formatted

--- Game/Program.cs
+++ Game/Program.cs
@@ -4,44 +4,48 @@ using SFML.Graphics;
 
 namespace Game
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            RenderWindow window = WindowOptions.Window;
-            WindowOptions.AddGlobalEvents(window);
+      RenderWindow window = WindowOptions.Window;
+      WindowOptions.AddGlobalEvents(window);
 
-            while (window.IsOpen) 
-            {
-                window.DispatchEvents();
-            }
-        }
+      while (window.IsOpen)
+      {
+        window.DispatchEvents();
+      }
     }
+  }
 
-    interface VisibleEntity {
-        void Render();
-        Visual Visual { get; set; }
-        bool Visible { get; set; } 
+  interface VisibleEntity
+  {
+    void Render();
+    Visual Visual { get; set; }
+    bool Visible { get; set; }
+  }
+
+  interface Visual
+  {
+    Color Color { get; set; }
+    Shape Shape { get; set; }
+    int X { get; set; }
+    int Y { get; set; }
+  }
+
+  class WindowOptions
+  {
+    public static uint Height = 600;
+    public static uint Width = 800;
+
+    public static VideoMode VMode = new VideoMode(Width, Height);
+    public static string Title = "Hello friend!";
+
+    public static RenderWindow Window = new RenderWindow(VMode, Title);
+
+    public static void AddGlobalEvents(Window window)
+    {
+      window.Closed += (sender, e) => { (sender as Window).Close(); };
     }
-
-    interface Visual {
-        Color Color { get; set; }
-        Shape Shape { get; set; }
-        int X { get; set; }
-        int Y { get; set; }
-    }
-
-    class WindowOptions {
-        public static uint Height = 600;
-        public static uint Width = 800;
-
-        public static VideoMode VMode = new VideoMode(Width, Height);
-        public static string Title = "Hello friend!";
-
-        public static RenderWindow Window = new RenderWindow(VMode, Title);
-
-        public static void AddGlobalEvents(Window window) {
-            window.Closed += (sender, e) => {(sender as Window).Close();};
-        }
-    }
+  }
 }
