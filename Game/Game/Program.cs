0931cf317c29b9e941f95d82812d40612b91ebfa
Adam Wallacessh-keygen -t rsa -b 4096 -C awallace689@gmail.com <awallace689@gmail.com>
2020-01-10 20:53:01 -0600

    init commit

---
+++ Game/Game/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using SFML.Window;
+using SFML.Graphics;
+
+namespace Game
+{
+    class Program
+    {
+        int count = 0;
+        static void Main(string[] args)
+        {
+            Window window = new Window(
+                new VideoMode(WindowOptions.Width, WindowOptions.Height), 
+                WindowOptions.Title
+            );
+
+            WindowOptions.AddGlobalEvents(window);
+
+            while (window.IsOpen) 
+            {
+                window.DispatchEvents();
+                
+            }
+        }
+    }
+
+    interface VisibleEntity {
+        void Render();
+        Visual Visual { get; set; }
+        bool Visible { get; set; } 
+    }
+
+    interface Visual {
+        Color Color { get; set; }
+        Shape Shape { get; set; }
+        int x { get; set; }
+        int y { get; set; }
+    }
+
+    class WindowOptions {
+        public static uint Height = 600;
+        public static uint Width = 800;
+        public static string Title = "Hello friend!";
+
+        public static void AddGlobalEvents(Window window) {
+            window.Closed += (sender, e) => {(sender as Window).Close();};
+        }
+    }
+}
